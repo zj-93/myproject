a724e3f354fe17fb27e656b405b0d19737ca349d
zhouJun <zhoujun_1993@163.com>
2019-04-16 21:30:07 +0800

    完成框架

---
+++ myProject/src/views/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="index">
+    <span>index</span>
+    <topMenu></topMenu>
+  </div>
+</template>
+
+<script>
+import topMenu from '@/views/topNav/topMenu.vue'
+export default {
+
+  components: {
+    topMenu: topMenu
+  },
+  data () {
+    return {
+    };
+  }
+}
+
+</script>
+<style lang='less' scoped>
+</style>
---
+++ myProject/src/views/topNav/topMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menu">
+      <div class="menuList">
+          <ul>
+              <li>美食</li>
+              <li>饮品</li>
+              <li>水果</li>
+              <li>旅游</li>
+          </ul>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sss',
+  data () {
+    return {
+        menuList: []
+    };
+  }
+}
+
+</script>
+<style lang='less' scoped>
+</style>
